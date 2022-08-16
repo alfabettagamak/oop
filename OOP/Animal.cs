39db02dbc3e8bfd9d2ae23cd5b97599974b3c4d5
alfabettagamak <alisa.skrynko@gmail.com>
2022-08-16 20:11:28 +0300

    commit

---
+++ OOP/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Animal
+{
+    public Animal(int age)
+    {
+        this.age = age;
+    }
+    
+    public Animal()
+    { }
+
+    private int age;
+    private int legs;
+    private bool isFur;
+
+    public Animal(int age, int legs, bool isFur)
+    {
+        this.age = age;
+        this.legs = legs;
+        this.isFur = isFur;
+    }
+
+    void say(string a)
+    {
+        Console.WriteLine(a);
+    }
+
+    bool eat()
+    {
+        Console.WriteLine("EAT!!!");
+        return true;
+    }
+    
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Runtime.Serialization.Json;
+
+Animal animal = new Animal();
+Animal animal1 = new Animal(8, 4, true);
+Animal animal2 = new Animal(15);
+
+addAnimal("Fedor", 8, 4.5);
+
+void addAnimal(string name, int age, double size)
+{
+    Console.WriteLine("добавит имя" + name);
+    Console.WriteLine("добавит возраст" + age);
+    Console.WriteLine("добавит размер" + size);
+}
+
+/*printSomething(false);
+sum(5, 89);
+Console.WriteLine(sum(5, 89));*/
+
+/*void printSomething(bool fromConsole)
+{
+    string str = "";
+    if (fromConsole)
+    {
+        Console.WriteLine("Input something");
+        str = Console.ReadLine();
+    }
+
+    Console.WriteLine("You print " + str);
+}
+
+
+int sum(int a, int b)
+{
+    var c = a + b;
+    return c;
+}*/
